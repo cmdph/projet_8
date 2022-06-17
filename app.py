ed216344f96dfaa2f5ff1e99d444c16dff0e8e8f
chawki <chawkiaminou@gmail.com>
2022-06-17 15:21:33 +0200

    init.

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import segmentation_models as sm
 from PIL import Image
-from app import app 
+from app import app
+from main import app
 import cv2
 
 
